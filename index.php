c0713c03e3a3be5213b94d07074d15e2b60b6419
Viniseeus <vinigolarte@gmail.com>
2023-04-04 21:15:42 -0300

    primeira versão

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FlipEnem</title>
+</head>
+<body>
+    <?php
+        echo "teste";
+    ?>
+</body>
+</html>
