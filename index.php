e002396f832824d421a4dc6a20b94d8d8145cc77
Viniseeus <vinigolarte@gmail.com>
2023-04-04 22:57:47 -0300

    adição de divs

--- index.php
+++ index.php
@@ -4,11 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+    <link rel = "stylesheet" type = "text/css" href = "estilos.css">
     <title>FlipEnem</title>
 </head>
 <body>
     <?php
-        echo "teste";
+       
     ?>
+ 
+    <div class="cabecalho">
+        <div class="logo"><a href="index.php">Flip Enem</a></div>
+    </div>
+   
+    <div class="container">
+          <div class="esquerda">Esquerda</div>
+          <div class="centro">
+                <button>Jogar</button>
+          </div>
+          <div class="direita">Direita</div>
+     </div> <!--Fechamento da classe container-->
+
 </body>
 </html>
