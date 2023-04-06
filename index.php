72251fb76380af251a2b3fefb60cf3a80daf1342
Viniseeus <vinigolarte@gmail.com>
2023-04-05 22:20:45 -0300

    modificações

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
-    <link rel = "stylesheet" type = "text/css" href = "estilos.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+   rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
+   crossorigin="anonymous"> -->
+    <link rel="icon" type="image/x-icon" href="icon.png">
+    <link rel ="stylesheet" type ="text/css" href ="estilos.css">
     <title>FlipEnem</title>
 </head>
 <body>
@@ -14,15 +17,21 @@
     ?>
  
     <div class="cabecalho">
-        <div class="logo"><a href="index.php">Flip Enem</a></div>
+        <div class="logo"><h1>Flip Enem</h1></a></div>
     </div>
    
     <div class="container">
-          <div class="esquerda">Esquerda</div>
-          <div class="centro">
-                <button>Jogar</button>
+          <div class="esquerda">
+                <p>teste</p>
           </div>
-          <div class="direita">Direita</div>
+         
+          <div class="centro">
+                <button onclick="Cronometragem.html" class="comecar">Jogar</a></button>
+          </div>
+         
+          <div class="direita">
+            <p>Direita</p>
+          </div>
      </div> <!--Fechamento da classe container-->
 
 </body>
