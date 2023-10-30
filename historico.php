314f919c852949daeb5b7b54e0e7e30c4f8b1453
Vinicius Leandro Pereira <viniciusleandropereira@gmail.com>
2023-10-30 14:19:26 -0300

    Versão fin3

--- historico.php
+++ historico.php
@@ -7,45 +7,30 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-
     <title>Histórico de Usuario</title>
     <style>
-        
         .lateral{
-            background-color: #081b29; 
+            background-color: rgba(207, 207, 207, 0.5); 
             height:100vh;           
         }
-        .titulo{
+        h1{
             font-size:30pt;
             text-align:center;
             margin-top: 3vh;
-            color:#00abf0;
         }
-        .centro{  
-            background-color: #081b29;
+        .centro{
+            background-color: rgba(207, 207, 207, 0.5);  
         }   
         .principal{
-            background-color: rgba(207, 207, 207, 0.1); 
-            height:78vh;    
-            border-radius: 5px;
+            background-color: rgba(207, 207, 207, 0.5); 
+            height:78vh;
+            
         } 
         p{
             font-size:15pt;
             text-align: center;
             margin-top:2vh;
             margin-bottom:2vh;
-            color:white;
-        }
-        .botaovoltar a{
-            text-decoration:none;
-            color:white;
-            font-size:50%;
-            margin-left:2%;
-        }
-        .botaovoltar a:hover{
-            color: #00abf0;
-            transition:0.5s;
         }
     </style>
 </head>
@@ -54,11 +39,13 @@
     <div class="row">
             <div class="col-1 lateral"></div>
                 <div class="col-10 centro">
-                     <h1 class="titulo">Meu histórico</h1>
-                     <p>Aqui ficará salvo o seu histórico de suas jogadas </p>
+                     <h1>Histórico</h1>
+                     <p>Aqui ficará o históricos de suas jogadas </p>
                 <div class="row">
                     <div class="col-12 principal">
-                    <h1 class="botaovoltar"><a href="home.html"><i class="bi bi-arrow-left"></i> Voltar</a></h1>
+                    <a href="index.php">
+                            <button>Voltar</button>
+                     </a>
                     </div>
                 </div>
                 </div>   
--- sobreprojeto.php
+++ sobreprojeto.php
@@ -7,45 +7,25 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-
     <title>Sobre o projeto FlipEnem</title>
     <style>
         .lateral{
-            background-color: #081b29;
+            background-color: rgba(207, 207, 207, 0.5); 
             height:100vh;           
         }
-        .sobre{
+        h1{
             font-size:30pt;
             text-align:center;
             margin-top: 3vh;
             margin-bottom: 3vh;
-            color:#00abf0;
         }
         .centro{
-            background-color: #081b29;
+            background-color: rgba(207, 207, 207, 0.5);
         }
         .principal{
-            background-color: rgba(207, 207, 207, 0.1); 
+            background-color: rgba(207, 207, 207, 0.5); 
             height:80vh;
             padding:2%;
-        }
-        .p {
-            font-size:15pt;
-            text-align: center;
-            margin-top:2vh;
-            margin-bottom:2vh;
-            color:white;
-        }
-        .botaovoltar a{
-            text-decoration:none;
-            color:white;
-            font-size:50%;
-            margin-left:2%;
-        }
-        .botaovoltar a:hover{
-            color: #00abf0;
-            transition:0.5s;
         }
       
         
@@ -57,14 +37,17 @@
     <div class="row">
             <div class="col-1 lateral"></div>
                 <div class="col-10 centro">
-                     <h1 class="sobre">Sobre o projeto Flip Enem</h1>
+                     <h1>Sobre o projeto Flip Enem</h1>
                     
                 <div class="row">
                     <div class="col-12 principal">
-                    <h1 class="botaovoltar"><a href="home.html"><i class="bi bi-arrow-left"></i> Voltar</a></h1>
-                    <p class="p">O FlipEnem foi um projeto iniciado no IFC - Sombrio, pelos alunos... </p>
-                    <p class="p">Ele recebeu este nome pelo fato de ser um fliperama com perguntas do Enem, cada aluno do IFC tinha acesso a uma conta e quando acessava, recebia uma pergunta aleatória e quando respondida tinha um resultado final para saber se acertou ou errou a pergunta</p>
-                </div>
+                    <p>O FlipEnem foi um projeto iniciado no IFC - Sombrio, pelos alunos... </p>
+                    <p>Ele recebeu este nome pelo fato de ser um fliperama com perguntas do Enem, cada aluno do IFC tinha acesso a uma conta e quando acessava, recebia uma pergunta aleatória e quando respondida tinha um resultado final para saber se acertou ou errou a pergunta</p>
+
+                    <a href="index.php">
+                            <button>Voltar</button>
+                     </a>
+                    </div>
                 </div>
                 </div>   
             <div class="col-1 lateral"></div>           
